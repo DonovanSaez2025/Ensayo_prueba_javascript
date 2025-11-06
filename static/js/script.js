6dcb27b844558d98c0a81d3781006cdf543bcaf2
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-11-06 14:06:59 -0300

    f
    f

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+/* 01. Pedir los datos de una persona (Nombre, apellidos, edad) 
+    y mostrarlos a través de una alerta. */
+function mostrarDatos() {
+    let nombre = prompt("Ingresa tu nombre: ");
+    let apellidos = prompt("Ingresa tus apellidos: ");
+    let edad = parseInt(prompt("Ingresa tu edad"));
+
+    if (isNaN(edad)) { // Verifica que la edad sea un número
+        alert("Ingrese una edad válida");
+    } else {
+        alert(`Hola señor ${nombre} ${apellidos}, veo que usted tiene ${edad} años.`);
+    };
+};
