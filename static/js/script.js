db02586b6f059f1d18f1d0f1973f7a0dc6c5bf18
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-11-06 15:15:08 -0300

    f
    f

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,140 @@
 
 /* 01. Pedir los datos de una persona (Nombre, apellidos, edad) 
     y mostrarlos a través de una alerta. */
-function saludar() {
+function mostrarDatos() {
     let nombre = prompt("Ingresa tu nombre: ");
     let apellidos = prompt("Ingresa tus apellidos: ");
     let edad = parseInt(prompt("Ingresa tu edad"));
 
-    if (isNaN(edad)) { // Verifica que la edad sea un número
-        alert("Ingrese una edad válida");
+    if (isNaN(edad) || nombre == "" || apellidos == "") { // Verifica que la edad sea un valor número y que nombre y apellidos no estén vacíos
+        alert("Ingrese datos válidos");
     } else {
         alert(`Hola señor ${nombre} ${apellidos}, veo que usted tiene ${edad} años.`);
     };
 };
 
+/* 02. Pedir un número, dividirlo en 3 partes, 
+sumar 10 y multiplicarlo por 2, luego mostrar el resultado en una alerta. */
+function calculoDificil() {
+    let num = parseInt(prompt("Ingresa un número: "));
+
+    if (isNaN(num)) {
+        alert("Ingrese un número válido.");
+    } else {
+        alert(`El resultado es: (${num} / 3 + 10) * 2 = ${(num / 3 + 10) * 2}`);
+    }
+};
+
+/* 03. Calcular la suma de los N primeros números naturales. 
+(N es ingresado por el usuario). Ejemplo: N=3 → alerta: 1 + 2 + 3 = 6 */
+function sumaNumeros() {
+    let num = parseInt(prompt("Ingresa un número: "));
+    let resultado = "El resultado es: 0";
+    let suma = 0;
+
+    if (isNaN(num)) {
+        alert("Ingrese un número válido.");
+    } else {
+        for (let i = 1; i <= num; i++) {
+            suma += i;
+            resultado += ` + ${suma}`;
+        }
+
+        alert(`${resultado} = ${suma}`);
+    };
+};
+
+/* 04. Mostrar en alerta los 100 primeros números pares. (Bucle) */
+function cienPrimerosPares() {
+    let resultado = "Los 100 primeros números pares son: \n";
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            resultado += ` ${i}`;
+        };
+    };
+
+    alert(resultado);
+};
+
+/* 05. Mostrar en alerta los 100 primeros números impares. (Bucle) */
+function cienPrimerosImpares() {
+    let resultado = "Los 100 primeros números impares son: \n";
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 != 0) {
+            resultado += ` ${i}`;
+        };
+    };
+
+    alert(resultado);
+};
+
+/* 06. Sumar 5 números leídos por teclado - Mostrar resultado (Bucle) */
+function sumarCincoNumeros() {
+    let resultado = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        let num = parseInt(prompt("Ingresa un número: "));
+
+        if (isNaN(num)) {
+            alert("Ingrese valores válidos.");
+            break; // Fuerza a que se rompa el bucle
+        } else {
+            resultado += num;
+        };
+    };
+
+    alert(`El resultado es ${resultado}.`);
+};
+
+/* 07. Modificar el anterior para que permita 
+sumar N números leídos por teclado. El valor de N se debe leer previamente por teclado. */
+function sumarMasNumeros() {
+    let limit = parseInt(prompt("Ingrese un número límite: "));
+    let resultado = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        let num = parseInt(prompt("Ingresa un número: "));
+
+        if (isNaN(num)) {
+            alert("Ingrese valores válidos.");
+            break;
+        } else {
+            resultado += num;
+        };
+    };
+
+    alert(`El resultado es ${resultado}.`);
+};
+
+/* 08. Sumar N números pares. (Leídos por teclado) */
+function sumarNumerosPares() {
+    let limit = parseInt(prompt("Ingrese un número límite: "));
+    let resultado = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        let num = parseInt(prompt("Ingresa un número: "));
+
+        if (isNaN(num) || num % 2 != 0) {
+            alert("Ingrese valores válidos y números pares.");
+            break;
+        } else {
+            resultado += num;
+        };
+    };
+
+    alert(`El resultado es ${resultado}.`);
+};
+
+/* 09. Sumar los N primeros pares. (Ej: N = 7 → Alerta: 2 + 4 + 6 = 12) */
+function sumarPrimerosPares() {
+    let limit = parseInt(prompt("Ingrese un número límite: "));
+    let resultado = "Los ${limit} primeros números pares son: ";
+
+    for (let i = 1; i <= num; i++) {
+        if (i % 2 == 0) {
+            resultado += ` ${i}`;
+        };
+    };
+
+    alert(resultado);
+};
